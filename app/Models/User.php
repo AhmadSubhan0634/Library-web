fe6d8d22c33b2ebba70f3903c86f78d7733f9c54
AhmadSubhan0634 <f240634@lhr.nu.edu.pk>
2026-07-27 17:02:02 +0500

    Make class of User

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,39 @@
 
 namespace App\Models;
 
-class User
-{
+class User{
+    private int $id;
+    private string $username;
+    private string $passwordHash;
+    private string $createdAt;
+    private string $updatedAt;
+
+    // Default constructor
+    public function __construct(
+        int $id = 0,
+        string $username = "",
+        string $passwordHash = "",
+        string $createdAt = "",
+        string $updatedAt = ""
+    ){
+        $this->id = $id;
+        $this->username = $username;
+        $this->passwordHash = $passwordHash;
+        $this->createdAt = $createdAt;
+        $this->updatedAt = $updatedAt;
+    }
+
+    // Getters
+    public function getId(): int { return $this->id; }
+    public function getUsername(): string { return $this->username; }
+    public function getPasswordHash(): string { return $this->passwordHash; }
+    public function getCreatedAt(): string { return $this->createdAt; }
+    public function getUpdatedAt(): string { return $this->updatedAt; }
+
+    // Setters
+    public function setId(int $id): void { $this->id = $id; }
+    public function setUsername(string $username): void { $this->username = $username; }
+    public function setPasswordHash(string $passwordHash): void { $this->passwordHash = $passwordHash; }
+    public function setCreatedAt(string $createdAt): void { $this->createdAt = $createdAt; }
+    public function setUpdatedAt(string $updatedAt): void { $this->updatedAt = $updatedAt; }
 }
